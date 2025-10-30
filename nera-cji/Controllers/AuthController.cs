cd62815579171780231c57f906260adaba36e473
Aleksander Ivanov <62203063+frontend-alex@users.noreply.github.com>
2025-10-30 10:41:31 +0100

    hotfix: authentication and root layout fixed and moved, project architecture fixed

---
+++ nera-cji/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace nera_cji.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
+
+
